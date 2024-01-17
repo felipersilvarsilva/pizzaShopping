5bc4902d848c52edb1cffcd9c9f713569fc627c2
felipe <106001741+felipersilvarsilva@users.noreply.github.com>
2024-01-17 00:25:37 +0000

    feat: Fix package.json dependencies, update auth layout, add sign-in page, create Input component and create Label component

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <div className="p-8">
+      <div className="w[350px] felx flex-col justify-center gap-4">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold">Acessar painel</h1>
+          <p className="text-sm text-muted-foreground ">
+            Acompanhe suas vendas pelo painel de parceiros
+          </p>
+        </div>
+
+        <form className="space-y-4">
+          <div className="space-y-2"></div>
+          <Button type="submit">Acessa painel</Button>
+        </form>
+      </div>
+    </div>
+  );
 }
