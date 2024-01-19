755479a8fc6e4744cda11bd1c1c29483421d50af
felipe <106001741+felipersilvarsilva@users.noreply.github.com>
2024-01-19 13:37:36 +0000

    feat: Fix package.json file, add Header component and add Separator component styling

---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import { Home, Pizza, UtensilsCrossed } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { NavLink } from "./nav-link";
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Pizza className="h6 w-6" />
+
+        <Separator orientation="vertical" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <Home className="h-4 w-4" />
+            Início
+          </NavLink>
+          <NavLink to="/orders">
+            <UtensilsCrossed className="h-4 w-4" />
+            Pedidos
+          </NavLink>
+        </nav>
+      </div>
+    </div>
+  );
+}
