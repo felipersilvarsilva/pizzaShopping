a14e3f2326bfd18910d5e65e12720cf60c7e078f
felipe <106001741+felipersilvarsilva@users.noreply.github.com>
2024-01-20 23:04:33 +0000

    feat: Format code, fix missing import statement, add Table component and add Orders page component

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,83 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ArrowRight, Search, X } from "lucide-react";
+
+export function Orders() {
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      </div>
+      <div className="space-y2.5">
+        <form className="flex items-center gap-2">
+          <span className="text-sm font-semibold">Filtros</span>
+          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
+        </form>
+        <div className="border rounded-md">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador </TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, i) => (
+                <TableRow key={i}>
+                  <TableCell>
+                    <Button variant="outline" size="xs">
+                      <Search className="h-3 w-3" />
+                      <span className="sr-only">Detalhes do pedido</span>
+                    </Button>
+                  </TableCell>
+                  <TableCell className="font-mono text-xs font-medium">
+                    1
+                  </TableCell>
+                  <TableCell className="text-muted-foreground">
+                    10:00 AM
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex items-center gap-2">
+                      <span className="h-2 w-2 rounded-full bg-slate-400" />
+                      <span className="font-medium text-muted-foreground">
+                        Pedente
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="font-medium">John Doe</TableCell>
+                  <TableCell className="font-medium">R$ 1.000,00</TableCell>
+                  <TableCell>
+                    <Button variant="outline" size="xs">
+                      <ArrowRight className="mr-2 h-3 w-3" />
+                      Aprovar
+                    </Button>
+                  </TableCell>
+                  <TableCell>
+                    <Button variant="ghost" size="xs">
+                      <X className="mr-2 h-3 w-3" />
+                      Cancelar
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </>
+  );
+}
