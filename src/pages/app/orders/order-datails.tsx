0d8cd385de38451eabb0e248f2f73739a7ca3534
felipe <106001741+felipersilvarsilva@users.noreply.github.com>
2024-01-21 05:43:20 +0000

    feat: Add Table component and format code and add status information to OrderDetails page

--- src/pages/app/orders/order-datails.tsx
+++ src/pages/app/orders/order-datails.tsx
@@ -4,6 +4,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
 export function OrderDetails() {
   return (
@@ -12,7 +13,23 @@ export function OrderDetails() {
         <DialogTitle>Pedido: 1</DialogTitle>
         <DialogDescription>Detalhes do Pedido</DialogDescription>
       </DialogHeader>
-      <div className="space-y-6"></div>
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-full bg-slate-400" />
+                  <span className="font-medium text-muted-foreground">
+                    Pedente
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </DialogContent>
   );
 }
